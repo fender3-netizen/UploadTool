ba0de1ce49617f586f36c5c2784239d3c6dc52c0
fender3-netizen <fender3@gmx.de>
2026-01-07 11:48:51 +0100

    Update functions.php

--- functions.php
+++ functions.php
@@ -9,20 +9,20 @@ function checkAdmin() {
     }
 }
 
-// Generiert zufälligen Token
+// Token generieren
 function generateToken($length=8){
     return substr(str_shuffle("abcdefghijklmnopqrstuvwxyz0123456789"), 0, $length);
 }
 
 // Prüft PDF Seitenmaße (nur erste Seite)
 function checkPDFSize($filePath, $minWidthMM, $minHeightMM){
-    require_once('vendor/autoload.php'); // Für FPDI / TCPDI
-    $pdf = new \setasign\Fpdi\Tcpdf\Fpdi();
+    require_once('vendor/autoload.php'); // FPDI
+
+    $pdf = new \setasign\Fpdi\Fpdi();
     $pageCount = $pdf->setSourceFile($filePath);
     $tpl = $pdf->importPage(1);
     $size = $pdf->getTemplateSize($tpl);
 
-    // Maße in mm
     $width = $size['width'];
     $height = $size['height'];
 
